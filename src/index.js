5cb4bf93f0ef266e41c38b986a4691739db97fa8
aatashj1 <frnkxoxo69@gmail.com>
2023-01-20 10:17:03 +0300

    test done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ if (str.length % 2 == 1) {
+  return false;
+ }
+ let beforeLength;
+ for (let i = 0; i < bracketsConfig.length; i++) {
+  let parfOfArr = bracketsConfig[i];
+  let malenkiuLychek = parfOfArr[0] + parfOfArr[1];
+  do {
+   beforeLength = str.length;
+   str = str.replaceAll(malenkiuLychek, "");
+   if(beforeLength>str.length){
+    i=-1;
+   }
+  }
+  while (beforeLength>str.length && str.length !== 0);
+
+  if (str.length===0) {
+   break;
+  }
+ }
+ if(str.length !== 0){
+  return false;
+ }
+ return true;
 }
+
